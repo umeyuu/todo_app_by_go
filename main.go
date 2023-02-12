a00e8baca3ff22497ebff16141c36e105f781d28
umeyuu <91179464+umeyuu@users.noreply.github.com>
2023-02-12 15:44:58 +0900

    make user table and insert

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"sqlite/go/src/todo_app_by_go/config"
+	"sqlite/go/src/todo_app_by_go/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbNmae)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+	// log.Println("test")
+
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
 }
