f411f6a538c4b1b39e85801d043c25ee3586209b
umeyuu <91179464+umeyuu@users.noreply.github.com>
2023-02-12 13:20:30 +0900

    set log

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"log"
+	"sqlite/go/src/todo_app_by_go/utils"
 
 	"gopkg.in/ini.v1"
 )
@@ -17,6 +18,7 @@ var Config ConfigList
 
 func init() {
 	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
 }
 
 func LoadConfig() {
