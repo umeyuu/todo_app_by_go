7f971445f43e3b69a6c1d745fdd43b6cbae51166
umeyuu <91179464+umeyuu@users.noreply.github.com>
2023-02-17 00:43:35 +0900

    add function logout

--- app/controllers/sever.go
+++ app/controllers/sever.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"sqlite/go/src/todo_app_by_go/app/models"
 	"sqlite/go/src/todo_app_by_go/config"
 )
 
@@ -16,6 +17,17 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 	templates.ExecuteTemplate(w, "layout", data)
 }
 
+func seesion(w http.ResponseWriter, r *http.Request) (sess models.Session, err error) {
+	cookie, err := r.Cookie("_cookie")
+	if err != nil {
+		sess = models.Session{UUID: cookie.Value}
+		if ok, _ := sess.CheckSession(); !ok {
+			err = fmt.Errorf("Invaild session")
+		}
+	}
+	return sess, err
+}
+
 func StartMainSever() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
@@ -24,5 +36,7 @@ func StartMainSever() error {
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/authenticate", authenticate)
+	http.HandleFunc("/todos", index)
+	http.HandleFunc("/logout", logout)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
