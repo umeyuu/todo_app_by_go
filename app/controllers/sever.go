ecb35f73d5b63732c3f4340a94801151c865a389
umeyuu <91179464+umeyuu@users.noreply.github.com>
2023-02-16 17:20:54 +0900

    add layout

--- app/controllers/sever.go
+++ app/controllers/sever.go
@@ -1,9 +1,20 @@
 package controllers
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 	"sqlite/go/src/todo_app_by_go/config"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainSever() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
